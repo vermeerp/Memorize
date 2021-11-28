0350bcb01e2821d35f9d6ca85e32052910e81c29
Pamela Vermeer <vermeerp@gmail.com>
2021-11-28 14:54:52 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Pamela Vermeer on 11/28/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
